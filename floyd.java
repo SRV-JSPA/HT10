85bceee9a37d79e50a98ad411b08d2aba181183f
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-05-15 18:40:18 -0600

    xd

--- floyd.java
+++ floyd.java
@@ -1,11 +1,28 @@
     // Java program for Floyd Warshall All Pairs Shortest
-// Path algorithm.
-import java.io.*;
-import java.lang.*;
+// Path algorithm
 import java.util.*;
-
 public class floyd {
 
+    public static int[][] shortestpath(int[][] adj, int[][] path) 
+    {
+        int n = adj.length;
+        int[][] ans = new int[n][n];
+        copy(ans, adj);
+    	for (int k=0; k<n;k++) 
+            for (int i=0; i<n; i++) 
+        	for (int j=0; j<n;j++) 
+                    if (ans[i][k]+ans[k][j] < ans[i][j]) {
+                        ans[i][j] = ans[i][k]+ans[k][j];
+          		        path[i][j] = path[k][j];
+                    }
+    	return ans;
+    }
+    public static void copy(int[][] a, int[][] b) 
+    {
+        for (int i=0;i<a.length;i++)
+            for (int j=0;j<a[0].length;j++)
+                a[i][j] = b[i][j];
+    }
 }
 
 
