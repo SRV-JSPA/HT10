31a7b4e9a12a67caf9252607b0c6cee0b545e4fe
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-05-16 20:00:05 -0600

    Update grafo.java

--- grafo.java
+++ grafo.java
@@ -3,10 +3,11 @@ import java.io.FileNotFoundException;
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Scanner;
+import java.util.regex.*;
 
 public class grafo {
     
+    List<String> lista = new ArrayList<String>();
     public void leer(){
         try (BufferedReader br = new BufferedReader(new FileReader(
             "logistica.txt"))) {
@@ -14,17 +15,21 @@ public class grafo {
         
         while ((caracter = br.readLine()) != null) {
 
-            String[] cadena = caracter.split("\\|");
-            for(int i=0; i<cadena.length;i++){
-                System.out.println(cadena[i]);
-            }
-
-        }
-
-    } catch (Exception e) {
+            String[] cadena = caracter.trim().split(",");
+           
+            for(int i = 0; i<cadena.length; i++){
+                lista.add(cadena[i]);
+            
+    } 
+    System.out.println(lista);   
+}}catch (Exception e) {
         System.out.println("Error reading file: " + e.getMessage());
         System.exit(1);
     }
 
+    }
+
+    public void separador(){
+        
     }
 }
