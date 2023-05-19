1c519a6e65b9ef8f38535a538e7e69b86751273e
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-05-18 21:31:17 -0600

    Menu

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.*;;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
@@ -6,5 +6,35 @@ public class Main{
 
         g.leer();
         g.separador();
+        Scanner sc = new Scanner(System.in);
+        boolean x = false;
+        while (!x) {
+            System.out.println("¡Bienvenido!    ");
+            System.out.println("Seleccione la opción que desee  ");
+            System.out.println("1. Ruta más corta entre dos ciudades    ");
+            System.out.println("2. Calcular el centro del grafo ");
+            System.out.println("3. Modificar el grafo   ");
+            System.out.println("4. Salir    ");
+            String y = sc.nextLine();
+            switch (y) {
+				case "1":
+					
+					break;
+				case "2":
+					
+					break;
+                case "3":
+					
+				break;
+				case "4":
+					System.out.println("¡Que tenga feliz día!");
+					x = true;
+					break;
+
+				default:
+					System.out.println("Por favor, ingrese una opción válida.");
+					break;
+			}
+        }
     }
 }
